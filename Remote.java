a8d3e43e78402bfbd6c98d7049f665b5851c3a70
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-02-02 22:21:56 +0600

    Lab 4

---
+++ Remote.java
@@ -0,0 +1,11 @@
+abstract class Remote {
+
+    TV tv1=new SmartTV();
+    TV tv2=new GeneralTV();
+
+    boolean tooglePower();
+    void volumeUp();
+    void volumeDown();
+    void channelUp();
+    void channelDown();
+}
